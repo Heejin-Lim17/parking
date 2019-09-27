e3dd4007106c7e18a91620170b84c81fe54481ea
HeejinLim17 <gmlwls6815@naver.com>
2019-09-27 15:30:07 +0900

    hi

--- app/src/main/java/kr/ac/gachon/parking/MainActivity.kt
+++ app/src/main/java/kr/ac/gachon/parking/MainActivity.kt
@@ -1,26 +1,85 @@
 package kr.ac.gachon.parking
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.navigation.NavigationView
+import androidx.core.view.GravityCompat
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
-import kotlinx.android.synthetic.main.app_bar_navigation_drawer.*
+import android.view.MenuItem
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.app_bar_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.app_bar_navigation_drawer)
-
+        setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-// 2. 툴바 왼쪽 버튼 설정
-        supportActionBar!!.setDisplayHomeAsUpEnabled(true) // 왼쪽 버튼 사용 여부 true
-        supportActionBar!!.setHomeAsUpIndicator(R.drawable.ic_toc_black_24dp) // 왼쪽 버튼 아이콘 설정
-        supportActionBar!!.setDisplayShowTitleEnabled(false) // 타이틀 안보이게 하기
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
 
+        val toggle = ActionBarDrawerToggle(
+            this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close
+        )
+        drawer_layout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        nav_view.setNavigationItemSelectedListener(this)
     }
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.activity_navigation_drawer_drawer, menu) // main_menu 메뉴를 toolbar 메뉴 버튼으로 설정
+
+    override fun onBackPressed() {
+        if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
+            drawer_layout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        when (item.itemId) {
+            R.id.action_settings -> return true
+            else -> return super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        // Handle navigation view item clicks here.
+        when (item.itemId) {
+            R.id.nav_groups-> {
+                // Handle the camera action
+            }
+            R.id.nav_my_group -> {
+
+            }
+            R.id.nav_dis_info -> {
+
+            }
+            R.id.nav_off_info -> {
+
+            }
+            R.id.nav_mem_info -> {
+
+            }
+            R.id.nav_logout -> {
+
+            }
+        }
+
+        drawer_layout.closeDrawer(GravityCompat.START)
         return true
     }
 }
